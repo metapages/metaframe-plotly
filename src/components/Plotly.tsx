cebd410b0d7ae74915ac60f0647ad4701f4944e4
automation <ci@oi.io>
2021-12-19 10:15:31 -0800

    Layout responsive fixes and output more plotly events

--- src/components/Plotly.tsx
+++ src/components/Plotly.tsx
@@ -1,6 +1,6 @@
 import { FunctionalComponent } from "preact";
-import { useEffect, useState } from "preact/hooks";
-import { useMetaframe } from "@metapages/metaframe-hook";
+import { useCallback, useEffect, useState } from "preact/hooks";
+import { useMetaframe, useHashParamBoolean } from "@metapages/metaframe-hook";
 import { MetaframeInputMap } from "@metapages/metapage";
 import Plot from "react-plotly.js";
 
@@ -11,6 +11,8 @@ export const Plotly: FunctionalComponent = () => {
   const [data, setData] = useState<any[]>([]);
   const [config, setConfig] = useState<any>({});
   const [layout, setLayout] = useState<any>({});
+  const [ debug ] = useHashParamBoolean("debug");
+
 
   useEffect(() => {
     if (metaframeObject.metaframe) {
@@ -42,13 +44,47 @@ export const Plotly: FunctionalComponent = () => {
     };
   }, [metaframeObject.metaframe, setData, setConfig, setLayout]);
 
+  const onError = useCallback((error :any) => {
+    if (!metaframeObject?.metaframe) {
+      return;
+    }
+    metaframeObject.metaframe.setOutputs({event: {
+      name: "error",
+      message: "plotly.js API method rejects",
+      data: error,
+    }});
+  }, [metaframeObject.metaframe]);
+
+  const onAfterPlot = useCallback(() => {
+    if (!metaframeObject?.metaframe) {
+      return;
+    }
+    metaframeObject.metaframe.setOutputs({event: {
+      name: "plotly_afterplot",
+    }});
+  }, [metaframeObject.metaframe]);
+
+  const onRedraw = useCallback(() => {
+    if (!metaframeObject?.metaframe) {
+      return;
+    }
+    metaframeObject.metaframe.setOutputs({event: {
+      name: "plotly_redraw",
+    }});
+  }, [metaframeObject.metaframe]);
+
   return (
     <Plot
       style={{ width: "100%", height: "100%" }}
       data={data}
-      layout={{ ...{ autosize: true }, ...layout }}
-      config={{ ...{ responsive: true }, ...config }}
+      layout={{ ...layout, ...{ autosize: true } }}
+      config={config }
       useResizeHandler={true}
+      debug={debug}
+      className={"maxsize"}
+      onError={onError}
+      onAfterPlot={onAfterPlot}
+      onRedraw={onRedraw}
     />
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { render } from "preact";
 import { WithMetaframe } from "@metapages/metaframe-hook";
 import { ChakraProvider } from "@chakra-ui/react";
 import { App } from "./App";
+import "./index.css";
 
 render(
   <ChakraProvider>
